9da943e754f8d21348c7facbc87cdc2b13c0918a
DailyStrength <jhkluck22@gmail.com>
2023-10-18 14:32:47 +0900

    struct

--- hongong_recursive/hongong_recursive/소스.cpp
+++ hongong_recursive/hongong_recursive/소스.cpp
@@ -38,19 +38,84 @@
 //	printf("-끝-");
 //}
 
-struct score
+//struct score
+//{
+//	int kor;
+//	int eng;
+//	int math;
+//};
+//
+//int main(void)
+//{
+//	struct score yuni = { 90, 80, 70 };
+//	struct score* ps = &yuni;
+//
+//	printf("국어 : %d\n", (*ps).kor);
+//	printf("영어 : %d\n", ps->eng);
+//	printf("수학 : %d\n", ps->math);
+//}
+
+//struct address
+//{
+//	char name[20];
+//	int age;
+//	char tel[20];
+//	char addr[80];
+//};
+//
+//int main(void)
+//{
+//	struct address list[5] = {
+//		{"홍길동", 23, "111-1111", "울릉도 독도"},
+//		{"이순신", 35, "222-2222", "서울 건천동"},
+//		{"장보고", 19, "333-3333", "완도 청해진"},
+//		{"유관순", 15, "444-4444", "충남 천안"},
+//		{"안중근", 45, "555-5555", "황해도 해주"}
+//	};
+//	int i;
+//
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%10s%5d%15s%20s\n",
+//			list[i].name, list[i].age, list[i].tel, list[i].addr);
+//	}
+//
+//	return 0;
+//}
+
+struct address
 {
-	int kor;
-	int eng;
-	int math;
+	char name[20];
+	int age;
+	char tel[20];
+	char addr[80];
 };
+
+void print_list(struct address* lp);
 
 int main(void)
 {
-	struct score yuni = { 90, 80, 70 };
-	struct score* ps = &yuni;
+	struct address list[5] = {
+		{"홍길동", 23, "111-1111", "울릉도 독도"},
+		{"이순신", 35, "222-2222", "서울 건천동"},
+		{"장보고", 19, "333-3333", "완도 청해진"},
+		{"유관순", 15, "444-4444", "충남 천안"},
+		{"안중근", 45, "555-5555", "황해도 해주"}
+	};
+	int i;
 
-	printf("국어 : %d\n", (*ps).kor);
-	printf("영어 : %d\n", ps->eng);
-	printf("수학 : %d\n", ps->math);
+	print_list(list);
+
+	return 0;
+}
+
+void print_list(struct address* lp)
+{
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		printf("%10s%5d%15s%20s\n",
+			(lp + i)->name, (lp + i)->age, (lp + i)->tel, (lp + i)->addr);
+	}
 }
